172929feae18ad2b9f1371d10be891bfe4676f01
hello-wn <hello_wn@yeah.net>
2019-01-18 11:37:53 +0800

    Remove unused variable

--- roman2Integer.py
+++ roman2Integer.py
@@ -5,7 +5,6 @@ class Solution:
         :rtype: int
         """
         origin = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
-        split_nums = {}
         s_list = list(s)
         res, last_char = 0, 0
         while len(s_list):
